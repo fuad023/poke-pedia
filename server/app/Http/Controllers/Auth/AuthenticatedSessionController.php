f8a38085e5dcd08fe3766d480c501257b455cbab
fuad <nafisfuadisc@gmail.com>
2026-04-07 22:35:18 +0600

    add: auth controllers

--- server/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ server/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -13,26 +13,26 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        $user = $request->user();
+        $token = $user->createToken('main')->plainTextToken;
 
-        return response()->noContent();
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
     }
 
     /**
-     * Destroy an authenticated session.
+     * Destroy an authenticated token.
      */
     public function destroy(Request $request): Response
     {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
         return response()->noContent();
     }
 }
