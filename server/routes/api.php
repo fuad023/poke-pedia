c83805551c89fd47dc0dce66c466b2238fbdc4c1
rashedulcse20230104022-hash <rashedul.cse.20230104022@aust.edu>
2026-04-08 08:44:30 +0600

    added Pokemon Search Page

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\PokemonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PokeController;
+
+Route::get('/pokemon', [PokeController::class, 'index']);
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
